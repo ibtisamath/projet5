d8fb329196b511a69fb0d4311e01f73de2241083
ibtisamath <144466642+ibtisamath@users.noreply.github.com>
2024-01-26 09:49:14 +0000

    Pending changes exported from your codespace

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,49 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const arrowleft = document.querySelector(".arrow_left");
+const arrowright = document.querySelector(".arrow_right");
+const img = document.querySelector(".banner-img");
+const txt = document.querySelector("p");
+let dots = document.querySelectorAll(".dot");
+const dotsContainer = document.querySelector(".dots");
+
+function dots1 () {
+    for (let i = 0; i< slides.length; i++) {
+	    const dot = document.createElement("div");
+	    dot.classList.add("dot");
+	    dotsContainer.appendChild(dot);
+	    const dots = document.querySelectorAll(".dot");
+		dots[0].classList.add("dot_selected");	
+   }
+}
+dots1();
+
+let i = 0;
+
+arrowright.addEventListener("click", function () {
+	const dots = document.querySelectorAll(".dot");
+	dots[i].classList.remove("dot_selected");
+	i++;
+	if (i === slides.length) {
+		i = 0
+	}
+	dots[i].classList.add("dot_selected");
+
+	img.src = "./assets/images/slideshow/" + slides[i].image;
+	txt.innerHTML = slides[i].tagLine;
+  });
+
+arrowleft.addEventListener("click", function () {
+	const dots = document.querySelectorAll(".dot");
+	dots[i].classList.remove("dot_selected");
+	i--;
+	if (i < 0) {
+	  i = slides.length -1
+	}
+	dots[i].classList.add("dot_selected");
+
+	img.src = "./assets/images/slideshow/" + slides[i].image;
+	txt.innerHTML = slides[i].tagLine;
+  });
